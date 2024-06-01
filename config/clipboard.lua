2f356db7ad9c5fd8bcfa8dffa1125ed3146454a3
Tobias Walle <tobias.walle@maibornwolff.de>
2024-06-01 11:19:18 +0200

    improve copy last output command

--- config/clipboard.lua
+++ config/clipboard.lua
@@ -4,12 +4,19 @@ local utils = require("utils")
 
 function M.copy_last_output_to_clipboard(window, pane)
 	local zones = pane:get_semantic_zones("Output")
-	local last_zone = zones[#zones]
-	if last_zone then
-		local text = pane:get_text_from_semantic_zone(last_zone)
-		window:copy_to_clipboard(utils.trim(text), "PrimarySelection")
-		pane:send_text("pbpaste | less\n")
+	-- Find the last zone with output and copy that
+	for i = #zones, 1, -1 do
+		local zone = zones[i]
+		if zone then
+			local text = utils.trim(pane:get_text_from_semantic_zone(zone))
+			if text and text ~= "" then
+				window:copy_to_clipboard(utils.trim(text), "PrimarySelection")
+				utils.notify("Copy Successful", "Copied: " .. text:sub(0, 10) .. "…")
+				return
+			end
+		end
 	end
+	utils.notify("Copy failed", "Couldn't find suitable output")
 end
 
 return M
