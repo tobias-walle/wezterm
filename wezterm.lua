caa89ba5d1c472faf18adc3c4dabb17da3d1b030
Tobias Walle <tobias.walle@maibornwolff.de>
2024-06-18 08:13:11 +0200

    wezterm: only open links with CTRL-Click to avoid accidental click

--- wezterm.lua
+++ wezterm.lua
@@ -132,6 +132,28 @@ config.mouse_bindings = {
 		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
 		mods = "NONE",
 	},
+
+	-- Change the default click behavior so that it only selects
+	-- text and doesn't open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("PrimarySelection"),
+	},
+
+	-- and make SHIFT-Click open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+
+	-- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.Nop,
+	},
 }
 
 -- General options
