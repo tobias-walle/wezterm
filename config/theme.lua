f84069f2b2aec28c6c95fc366aa93ddb8c24d754
Tobias Walle <git@tobias.walle.email>
2024-07-05 08:34:59 +0200

    wezterm: fix colors with aider

--- config/theme.lua
+++ config/theme.lua
@@ -7,12 +7,15 @@ local wezterm = require("wezterm")
 function M.configure_theme(config, theme_name)
 	M.colors = require("plugins.catppuccin").get_colors(theme_name)
 
-	config.color_scheme = theme_name
-	M.theme = wezterm.color.get_builtin_schemes()[config.color_scheme]
+	M.theme = wezterm.color.get_builtin_schemes()[theme_name]
+	M.theme.indexed[16] = nil
 	M.theme.tab_bar.inactive_tab.bg_color = M.theme.tab_bar.background
 	M.theme.tab_bar.inactive_tab.fg_color = M.colors.surface2
 	M.theme.tab_bar.active_tab.fg_color = M.colors.mauve
 	M.theme.tab_bar.active_tab.bg_color = M.theme.tab_bar.background
+
+	config.color_schemes = { ["Custom"] = M.theme }
+	config.color_scheme = "Custom"
 end
 
 return M
